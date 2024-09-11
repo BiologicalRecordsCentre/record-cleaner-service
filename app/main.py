a600941452a43206b009bc583457c25cac03cf1f
Jim Bacon <jame2@ceh.ac.uk>
2024-09-11 13:37:08 +0000

    Bump version

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app = FastAPI(
     title="Record Cleaner Service",
     summary="Service for checking species records against the record "
     "cleaner rules.",
-    version="0.0.15",
+    version="0.0.16",
     contact={
         "name": "Biological Records Centre at UK Centre for Ecology & Hydrology",
         "url": "https://www.ceh.ac.uk/our-science/projects/biological-records-centre",
