BiologicalRecordsCentre/record-cleaner-service

commit a2e4f0ed831ded7968c34988c1f274cbef4db6c8

Do vice county assignment in validation
Fixes #5 and #10

app/county/county_routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from app.auth import get_current_user
 from app.utility.vice_county.vc_checker import VcChecker
@@ -15,20 +15,35 @@ router = APIRouter(
 @router.get(
     "/code/{code}",
     summary="Get county with given code.")
-async def read_county_by_code( code: str | int):
+async def read_county_by_code(code: str | int):
 
-    return {
-        "code": code,
-        "county": VcChecker.prepare_code(code)
-    }
+    try:
+        code = VcChecker.prepare_code(code)
+        return {
+            "code": code,
+            "name": VcChecker.get_name_from_code(code)
+        }
+    except ValueError:
+        raise HTTPException(
+            status_code=404, detail="No county found for code.")
 
 
 @router.get(
-    '/name/{name}',
-    summary="Get county with given name.",)
-async def read_county_by_name(name: str):
+    '/gridref/{gridref}',
+    summary="Get county containing gridref.")
+async def read_county_by_gridref(gridref: str):
+    """Returns the primary county for the gridref but may be incorrect when the
+    square straddles more than one county. The gridref must be well formed with
+    capital letters and no spaces."""
 
-    return {
-        "code": VcChecker.prepare_code(name),
-        "county": name
-    }
+    try:
+        gridref = VcChecker.prepare_sref(gridref)
+        code = VcChecker.get_code_from_sref(gridref)
+        return {
+            "gridref": gridref,
+            "code": code,
+            "name": VcChecker.get_name_from_code(code)
+        }
+    except ValueError:
+        raise HTTPException(
+            status_code=404, detail="No county found for gridref.")

app/routes.py
@@ -7,6 +7,7 @@ from app.auth import AdminDependency, get_current_admin_user
 import app.main as app
 # from app.county.county_routes import router as county_router
 from app.auth import router as auth_router
+from app.county.county_routes import router as county_router
 from app.rule.rule_routes import router as rule_router
 from app.settings import SettingsDependency
 from app.species.species_routes import router as species_router
@@ -50,7 +51,7 @@ router.include_router(species_router)
 router.include_router(user_router)
 router.include_router(validate_router)
 router.include_router(verify_router)
-# router.include_router(county_router)
+router.include_router(county_router)
 
 
 @router.get(

app/utility/vice_county/vc_checker.py
@@ -5,6 +5,10 @@ from functools import lru_cache
 import pandas as pd
 
 
+class NoVcException(Exception):
+    pass
+
+
 class VcChecker:
 
     # Private class variables.
@@ -21,7 +25,10 @@ class VcChecker:
                 f'{basedir}/vc_names.csv',
                 sep=',',
                 names=['name', 'code'],
-                converters={'name': lambda x: x.lower()}
+            )
+            # Add a simplified name for searching.
+            cls.__vc_names['search_name'] = (
+                cls.__vc_names['name'].str.lower().str.replace(' ', '')
             )
 
         # Load the list that relates grid squares to vice counties.
@@ -36,7 +43,7 @@ class VcChecker:
     @classmethod
     @lru_cache
     def prepare_code(cls, value: str | int) -> str:
-        """Takes a supplied VC value and returns a code."""
+        """Takes a supplied VC name or code and returns a clean code."""
 
         # British codes may be passes as int or str.
         try:
@@ -63,11 +70,11 @@ class VcChecker:
                 raise ValueError('Unrecognised vice county value.')
 
         # Search the VC names for a match if value was not a code.
-        seek = value.lower()
+        seek = value.lower().replace(' ', '')
         df = cls.__vc_names
         # Locate in the dataframe of __vc_names, the code where the name
         # matches the entered value.
-        series = df.loc[df['name'] == seek, 'code']
+        series = df.loc[df['search_name'] == seek, 'code']
         if series.size == 1:
             return series.iloc[0]
         else:
@@ -75,7 +82,9 @@ class VcChecker:
 
     @classmethod
     def prepare_sref(cls, value: str) -> str:
-        """Takes a supplied sref and returns a 10km, 2km, or 1km square."""
+        """Takes a supplied sref and returns a 10km, 2km, or 1km square.
+
+        The sref should already have been validated."""
 
         # Remove the 100km square indicator. Maybe 1 or 2 characters.
         if value[1].isdigit():
@@ -97,6 +106,36 @@ class VcChecker:
                 return prefix + suffix[0:2] + suffix[4:6]
             case 10:
                 return prefix + suffix[0:2] + suffix[5:7]
+            case _:
+                raise ValueError('Unrecognised gridref.')
+
+    @classmethod
+    @lru_cache
+    def get_name_from_code(cls, code: str) -> str:
+        """Takes a code and returns the name of the VC.
+
+        The code should be a value as output by prepare_code."""
+
+        df = cls.__vc_names
+        series = df.loc[df['code'] == code, 'name']
+        if series.size == 1:
+            return series.iloc[0]
+        else:
+            raise ValueError('No vice county found for code.')
+
+    @classmethod
+    @lru_cache
+    def get_code_from_sref(cls, gridref: str) -> str:
+        """Takes a gridref and returns the code of the VC.
+
+        The sref should be a value as output by prepare_sref."""
+        df = cls.__vc_squares
+        series = df.loc[df['gridref'] == gridref, 'vc_dominant']
+        if series.size == 1:
+            return str(series.iloc[0])
+        else:
+            # Raise a special exception as this is not an error.
+            raise NoVcException('No vice county found for gridref.')
 
     @classmethod
     @lru_cache

app/validate/validate_routes.py
@@ -7,7 +7,7 @@ from app.rule.difficulty.difficulty_rule_repo import DifficultyRuleRepo
 from app.settings_env import EnvDependency
 import app.species.cache as cache
 from app.utility.sref.sref_factory import SrefFactory
-from app.utility.vice_county.vc_checker import VcChecker
+from app.utility.vice_county.vc_checker import VcChecker, NoVcException
 from app.utility.vague_date import VagueDate
 
 from .validate_models import Validate, Validated
@@ -33,7 +33,7 @@ async def validate(
 
     The spatial reference can be given as a grid reference, a latitude and
     longitude or an easting and northing. In the latter two cases, an accuracy
-    is also required. The srid parameter must match the spatial reference 
+    is also required. The srid parameter must match the spatial reference
     system in which the coordinates are given.
 
     Supported systems are:
@@ -49,8 +49,8 @@ async def validate(
         # Our response begins with the input data.
         result = Validated(**record.model_dump())
 
-        # 1. Confirm TVK/name is valid.
         try:
+            # 1. Confirm TVK/name is valid.
             if record.tvk is None and record.name is None:
                 result.ok = False
                 result.messages.append("TVK or name required.")
@@ -73,13 +73,12 @@ async def validate(
                 # Get id difficulty.
                 repo = DifficultyRuleRepo(db, env)
                 result.id_difficulty = repo.run(result)
-
         except Exception as e:
             result.ok = False
             result.messages.append(str(e))
 
-        # 2. Confirm date is valid.
         try:
+            # 2. Confirm date is valid.
             vague_date = VagueDate(record.date)
             # Return date in preferred format.
             result.date = str(vague_date)
@@ -87,21 +86,28 @@ async def validate(
             result.ok = False
             result.messages.append(str(e))
 
-        # 3. Confirm sref is valid.
         try:
+            # 3. Confirm sref is valid.
             sref = SrefFactory(record.sref)
-            if record.sref.gridref is not None:
-                # Return cleaned up gridref.
-                result.sref.gridref = sref.gridref
+            # Include gridref in result.
+            result.sref.gridref = sref.gridref
 
-            # Check sref in vice county.
+            # 4a. Either check vice county...
             if record.vc is not None:
                 code = VcChecker.prepare_code(record.vc)
                 # Return code if name was supplied.
                 result.vc = code
                 gridref = VcChecker.prepare_sref(sref.gridref)
                 VcChecker.check(gridref, code)
+            # 4b. Or assign vice county.
+            else:
+                gridref = VcChecker.prepare_sref(sref.gridref)
+                result.vc = VcChecker.get_code_from_sref(gridref)
 
+        except NoVcException as e:
+            # Failure to assign is not an error.
+            result.vc = '0'
+            result.messages.append(str(e))
         except Exception as e:
             result.ok = False
             result.messages.append(str(e))

test/county/test_county.py
@@ -1,0 +1,31 @@
+from fastapi.testclient import TestClient
+
+
+class TestCounty:
+
+    def test_county_by_code(self, client: TestClient):
+        # Valid code.
+        response = client.get('/county/code/26')
+        assert response.status_code == 200
+
+        county = response.json()
+        assert county['code'] == '26'
+        assert county['name'] == 'West Suffolk'
+
+        # Invalid code.
+        response = client.get('/county/code/999')
+        assert response.status_code == 404
+
+    def test_county_by_gridref(self, client: TestClient):
+        # Valid gridref.
+        response = client.get('/county/gridref/TL123456')
+        assert response.status_code == 200
+
+        county = response.json()
+        assert county['gridref'] == 'TL1245'
+        assert county['code'] == '30'
+        assert county['name'] == 'Bedfordshire'
+
+        # Invalid gridref.
+        response = client.get('/county/gridref/TM9999')
+        assert response.status_code == 404

test/validate/test_validate.py
@@ -308,3 +308,60 @@ class TestValidate:
         assert validated['vc'] == "30"
         assert validated['ok']
         assert len(validated['messages']) == 0
+
+    def test_vc_assignment(self, client: TestClient, mocker):
+        # Mock the Indicia warehouse.
+        mocker.patch(
+            'app.species.indicia.make_search_request',
+            mock_make_search_request
+        )
+
+        response = client.post(
+            "/validate",
+            json=[{
+                "id": 1,
+                "date": "3/4/2024",
+                "sref": {
+                    "srid": 0,
+                    "gridref": "TL 123 456"
+                },
+                "tvk": "NBNSYS0000008319"
+            }]
+        )
+        assert response.status_code == 200
+        validated = response.json()[0]
+        assert validated['name'] == "Adalia bipunctata"
+        assert validated['date'] == "03/04/2024"
+        assert validated['sref']['gridref'] == "TL123456"
+        assert validated['vc'] == "30"
+        assert validated['ok']
+        assert len(validated['messages']) == 0
+
+    def test_vc_no_assignment(self, client: TestClient, mocker):
+        # Mock the Indicia warehouse.
+        mocker.patch(
+            'app.species.indicia.make_search_request',
+            mock_make_search_request
+        )
+
+        response = client.post(
+            "/validate",
+            json=[{
+                "id": 1,
+                "date": "3/4/2024",
+                "sref": {
+                    "srid": 0,
+                    "gridref": "TM 999 999"
+                },
+                "tvk": "NBNSYS0000008319"
+            }]
+        )
+        assert response.status_code == 200
+        validated = response.json()[0]
+        assert validated['name'] == "Adalia bipunctata"
+        assert validated['date'] == "03/04/2024"
+        assert validated['sref']['gridref'] == "TM999999"
+        assert validated['vc'] == '0'
+        assert validated['ok']
+        assert len(validated['messages']) == 1
+        assert validated['messages'][0] == "No vice county found for gridref."

commit cc940ce36683c30f2d01afda9155f93bfb407b12

Fix bug introduced in a2e4f0ed
Exception raised by error had changed.

app/county/county_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.auth import get_current_user
-from app.utility.vice_county.vc_checker import VcChecker
+from app.utility.vice_county.vc_checker import VcChecker, NoVcException
 
 
 router = APIRouter()
@@ -23,9 +23,9 @@ async def read_county_by_code(code: str | int):
             "code": code,
             "name": VcChecker.get_name_from_code(code)
         }
-    except ValueError:
+    except ValueError as e:
         raise HTTPException(
-            status_code=404, detail="No county found for code.")
+            status_code=404, detail=str(e))
 
 
 @router.get(
@@ -44,6 +44,6 @@ async def read_county_by_gridref(gridref: str):
             "code": code,
             "name": VcChecker.get_name_from_code(code)
         }
-    except ValueError:
+    except NoVcException as e:
         raise HTTPException(
-            status_code=404, detail="No county found for gridref.")
+            status_code=404, detail=str(e))