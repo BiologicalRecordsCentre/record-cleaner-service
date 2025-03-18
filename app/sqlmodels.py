623b1255314d452bb871698c1d07d040da50e854
Jim Bacon <jame2@ceh.ac.uk>
2025-03-18 09:49:09 +0000

    Add names to database constraints

--- app/sqlmodels.py
+++ app/sqlmodels.py
@@ -3,6 +3,17 @@ from typing import Optional
 from sqlmodel import Field, SQLModel, UniqueConstraint
 
 
+# Create a naming convention. Without this objects may get created without
+# a name in SQLite, preventing them from being updated by Alembic.
+SQLModel.metadata.naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
 class System(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     key: str = Field(index=True, unique=True)
