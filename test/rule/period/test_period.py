c568a1e321c2ea947191db624b61e5d69e2c732d
Jim Bacon <jame2@ceh.ac.uk>
2026-01-15 10:22:55 +0000

    Add test for optional period dates

--- test/rule/period/test_period.py
+++ test/rule/period/test_period.py
@@ -75,3 +75,93 @@ class TestPeriodRule:
             assert result[0]['group'] == 'group1'
             assert result[0]['start_date'] == '2020-01-01'
             assert result[0]['end_date'] == '2020-12-31'
+
+    def test_period_rule_no_start_date(self, client: TestClient):
+        # Get database connection from client.
+        engine = client.app.context['engine']
+        with Session(engine) as db:
+            # Create org_group.
+            org_group = OrgGroup(organisation='organisation1', group='group1')
+            db.add(org_group)
+            db.commit()
+            db.refresh(org_group)
+
+            # Create taxon.
+            taxon = Taxon(
+                name='Adalia bipunctata',
+                preferred_name='Adalia bipunctata',
+                search_name='adaliabipunctata',
+                tvk='NBNSYS0000008319',
+                preferred_tvk='NBNSYS0000008319',
+                preferred=True,
+                organism_key='NBNORG0000010513',
+            )
+            db.add(taxon)
+            db.commit()
+            db.refresh(taxon)
+
+            # Create period rule.
+            period_rule = PeriodRule(
+                org_group_id=org_group.id,
+                organism_key=taxon.organism_key,
+                taxon=taxon.name,
+                end_date=date(2020, 12, 31)
+            )
+            db.add(period_rule)
+            db.commit()
+            db.refresh(period_rule)
+
+            # Request period rules for org_group.
+            response = client.get(
+                f'/rules/period/org_group/{org_group.id}')
+            assert response.status_code == 200
+            result = response.json()
+            assert result[0]['organism_key'] == 'NBNORG0000010513'
+            assert result[0]['taxon'] == 'Adalia bipunctata'
+            assert result[0]['start_date'] is None
+            assert result[0]['end_date'] == '2020-12-31'
+
+    def test_period_rule_no_end_date(self, client: TestClient):
+        # Get database connection from client.
+        engine = client.app.context['engine']
+        with Session(engine) as db:
+            # Create org_group.
+            org_group = OrgGroup(organisation='organisation1', group='group1')
+            db.add(org_group)
+            db.commit()
+            db.refresh(org_group)
+
+            # Create taxon.
+            taxon = Taxon(
+                name='Adalia bipunctata',
+                preferred_name='Adalia bipunctata',
+                search_name='adaliabipunctata',
+                tvk='NBNSYS0000008319',
+                preferred_tvk='NBNSYS0000008319',
+                preferred=True,
+                organism_key='NBNORG0000010513',
+            )
+            db.add(taxon)
+            db.commit()
+            db.refresh(taxon)
+
+            # Create period rule.
+            period_rule = PeriodRule(
+                org_group_id=org_group.id,
+                organism_key=taxon.organism_key,
+                taxon=taxon.name,
+                start_date=date(2020, 1, 1),
+            )
+            db.add(period_rule)
+            db.commit()
+            db.refresh(period_rule)
+
+            # Request period rules for org_group.
+            response = client.get(
+                f'/rules/period/org_group/{org_group.id}')
+            assert response.status_code == 200
+            result = response.json()
+            assert result[0]['organism_key'] == 'NBNORG0000010513'
+            assert result[0]['taxon'] == 'Adalia bipunctata'
+            assert result[0]['start_date'] == '2020-01-01'
+            assert result[0]['end_date'] is None
