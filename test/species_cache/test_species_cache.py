348231da4d48fa1c1db27e01dd3d742b0c756826
Jim Bacon <jame2@ceh.ac.uk>
2026-03-05 17:35:06 +0000

    Add tests for new caching

--- test/species_cache/test_species_cache.py
+++ test/species_cache/test_species_cache.py
@@ -1,6 +1,12 @@
 import json
 
 from fastapi.testclient import TestClient
+from sqlmodel import Session
+
+from app.settings_env import EnvSettings
+from app.species.cache import (
+    get_taxon_by_tvk, get_taxon_by_name, _get_taxon_by_tvk_wrapped
+)
 
 from ..mocks import mock_make_search_request
 
@@ -18,6 +24,11 @@ class TestSpeciesCache:
         assert response.status_code == 200
         result = response.json()
         assert result['count'] == 0
+
+        # Empty LRU Cache.
+        # If the LRU cache contains this record already then it won't be added
+        # to the cache table.
+        _get_taxon_by_tvk_wrapped.cache_clear()
 
         # Request species by TVK.
         response = client.get("/species/cache/taxon_by_tvk/NBNSYS0000008319")
@@ -68,3 +79,27 @@ class TestSpeciesCache:
         assert response.status_code == 404
         result = json.loads(response.text)
         assert result['detail'] == 'TVK ABC123 not recognised.'
+
+    def test_lru_cache_by_tvk(self, db: Session, env: EnvSettings, mocker):
+        # Mock the Indicia warehouse.
+        mocker.patch(
+            'app.species.indicia.make_search_request',
+            mock_make_search_request
+        )
+        # First request should look up taxon.
+        taxon1 = get_taxon_by_tvk(db, env, 'NBNSYS0000008319')
+        # Second request should hit LRU cache.
+        taxon2 = get_taxon_by_tvk(db, env, 'NBNSYS0000008319')
+        assert taxon1 == taxon2
+
+    def test_lru_cache_by_name(self, db: Session, env: EnvSettings, mocker):
+        # Mock the Indicia warehouse.
+        mocker.patch(
+            'app.species.indicia.make_search_request',
+            mock_make_search_request
+        )
+        # First request should look up taxon.
+        taxon1 = get_taxon_by_name(db, env, 'Adalia bipunctata')
+        # Second request should hit LRU cache.
+        taxon2 = get_taxon_by_name(db, env, 'Adalia bipunctata')
+        assert taxon1 == taxon2
